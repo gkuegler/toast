7b77fe6f510efe0bb3ded36a47015dd86909a763
George Kuegler <georgekuegler@gmail.com>
2023-11-05 11:42:42 -0500

    progress

--- toast.cpp
+++ toast.cpp
@@ -1,16 +1,20 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-#pragma warnging(push)
+#pragma warning(push)
 #pragma warning(disable : 4996)
-#include <wx/wx.h>
+#include <wx/wxh>
 #include <wx/cmdline.h>
-#pragma warnging(pop)
+#pragma warning(pop)
+
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/basic_file_sink.h"
 
 void
 DrawText(wxPaintDC& dc, wxString text, wxPoint pt)
 {
-  wxFont font(12, wxFONTFAMILY_SWISS, wxNORMAL, wxBOLD);
+  //wxFont font(12, wxFONTFAMILY_SWISS, wxNORMAL, wxBOLD);
+  wxFont font = wxFontInfo(12).FaceName("Helvetica");
   dc.SetFont(font);
    //dc.SetBackgroundMode(wxTRANSPARENT);
   dc.SetTextForeground(*wxBLACK);
@@ -47,11 +51,66 @@ public:
 class Frame : public wxFrame
 {
 public:
+  wxStaticText title;
+  wxStaticText body;
+
   Frame(wxPoint p, wxSize s)
-    : wxFrame(nullptr, wxID_ANY, "Example Title", p, s, wxDEFAULT_FRAME_STYLE)
+    : wxFrame(nullptr, wxID_ANY, "Toast - Hello World!", p, s, wxDEFAULT_FRAME_STYLE)
   {
-     auto panel = new Panel(this);
+    auto panel = new wxPanel(this);
     //// auto panel = new wxPanel(this, size=GetClientRect().GetSize());
+    auto COLOR_WHITE = wxColour(255, 255, 255);
+    // COLOR_YELLOW = wxColour(250, 247, 190)
+    // COLOR_YELLOW = wxColour(245, 240, 140)
+    auto COLOR_YELLOW = wxColour(245, 239, 118);
+    auto COLOR_BLACK = wxColour(0, 0, 0);
+    
+    panel -> SetSize(GetClientSize());
+    panel -> SetBackgroundColour(COLOR_BLACK)
+
+    // timer = wxTimer(self)
+    // timer.StartOnce(milliseconds=2000)
+    // timer.StartOnce(milliseconds=5000)
+
+    auto font = wxFont(18, family = wxFONTFAMILY_DEFAULT, style = 0, weight = 90, 
+                  underline = False, faceName ="Consolas", encoding = wxFONTENCODING_DEFAULT)
+
+    
+    title = new wxStaticText(panel, style=wxALIGN_LEFT);
+    title -> SetFont(font);
+    title -> SetBackgroundColour(COLOR_BLACK);
+    title -> SetForegroundColour(COLOR_YELLOW);
+    title -> SetLabel("hello");
+
+    body = new wxStaticText(panel, style=wxALIGN_LEFT);
+    body -> SetFont(font);
+    body -> SetBackgroundColour(COLOR_BLACK);
+    body -> SetForegroundColour(COLOR_WHITE);
+    body -> SetLabel("world");
+
+
+    // May not be necessary to bind this to the panel
+    // if both the title and body controls take up the entire
+    // client area of the panel.
+    // panel.Bind(wxEVT_LEFT_UP, close)
+
+    title -> Bind(wxEVT_LEFT_UP, close);
+    body -> Bind(wxEVT_LEFT_UP, close);
+    Bind(wxEVT_TIMER, close);
+
+    auto sz_main = new wxBoxSizer(wxVERTICAL);
+    sz_main -> Add(title, 0, wxEXPAND | wxBOTTOM, 5);
+    sz_main -> Add(body, 1, wxEXPAND, 0);
+
+    sub_sizer = new wxBoxSizer(wxVERTICAL);
+    sub_sizer -> Add(sz_main, 1, wxEXPAND | wxALL, 5);
+    panel -> SetSizer(sub_sizer);
+    
+    // Also a weird problem where the panel doesn't
+    // fll to the size of the frame.
+    panel -> Layout();
+
+
 
      auto* top = new wxBoxSizer(wxVERTICAL);
      top->Add(panel, 1, wxEXPAND, 0);
@@ -125,8 +184,11 @@ public:
 
   bool OnCmdLineParsed(wxCmdLineParser& parser)
   {
-      //silent_mode = parser.Found(wxT("s"));
-      wxMessageBox("parsing args...");
+
+      spdlog::debug("parsing args...");
+      bool silent_mode = parser.Found(wxT("s"));
+      spdlog::debug("silent mode found: {}", silent_mode);
+
 
       // to get at your unnamed parameters use
       wxArrayString args;
@@ -138,7 +200,7 @@ public:
       // and other command line parameters
       for (auto& arg : args) {
           std::cout << arg << std::endl;
-          wxMessageBox(arg);
+          //spdlog::debug("argument: {}", arg.c_str());
       }
 
       // then do what you need with them.
@@ -148,6 +210,11 @@ public:
   }
   virtual bool OnInit()
   {
+      auto logger = spdlog::basic_logger_mt("basic_logger", "app.log");
+      logger->set_level(spdlog::level::debug);
+      logger->debug("Logging to file!");
+      spdlog::set_default_logger(std::move(logger));
+      spdlog::flush_every(std::chrono::seconds(2));
       auto size = wxSize(300, 150);
       frame_ = new Frame(CalcOrigin(size), size);
       frame_->Show();
