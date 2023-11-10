1f41971e2bf6323b32c63fbc02909af994a1cf4c
George Kuegler <georgekuegler@gmail.com>
2023-11-09 23:00:00 -0500

    capture arguments

--- toast.cpp
+++ toast.cpp
@@ -13,6 +13,10 @@
 #include <spdlog/spdlog.h>
 #include "spdlog/sinks/basic_file_sink.h"
 
+std::string convert(wxString text) {
+    return std::string(text.c_str());
+}
+
 void
 DrawText(wxPaintDC& dc, wxString text, wxPoint pt)
 {
@@ -54,10 +58,10 @@ public:
 class Frame : public wxFrame
 {
 public:
-  wxStaticText* title;
+  wxStaticText* text_title;
   wxStaticText* body;
 
-  Frame(wxPoint p, wxSize s)
+  Frame(wxPoint p, wxSize s, wxString message, wxString title)
     : wxFrame(nullptr, wxID_ANY, "Toast - Hello World!", p, s, wxDEFAULT_FRAME_STYLE)
   {
     auto panel = new wxPanel(this);
@@ -75,21 +79,21 @@ public:
     // timer.StartOnce(milliseconds=2000)
     // timer.StartOnce(milliseconds=5000)
 
-    // auto font = wxFont(18, wxFONTFAMILY_DEFAULT, 0, 90, false, "Consolas", wxFONTENCODING_DEFAULT);
+     //auto font = wxFont(18, wxFONTFAMILY_DEFAULT, 0, 90, false, "Consolas", wxFONTENCODING_DEFAULT);
 
     
     //title = new wxStaticText(panel, wxALIGN_LEFT);
-    title = new wxStaticText(panel, wxID_ANY, "xxx", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    text_title = new wxStaticText(panel, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
     // title -> SetFont(font);
-    title -> SetBackgroundColour(COLOR_BLACK);
-    title -> SetForegroundColour(COLOR_YELLOW);
-    title -> SetLabel("hello");
+    text_title-> SetBackgroundColour(COLOR_BLACK);
+    text_title-> SetForegroundColour(COLOR_YELLOW);
+    //text_title-> SetLabel("hello");
 
-    body = new wxStaticText(panel, wxID_ANY, "xxx", wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    body = new wxStaticText(panel, wxID_ANY, message, wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
     // body -> SetFont(font);
     body -> SetBackgroundColour(COLOR_BLACK);
     body -> SetForegroundColour(COLOR_WHITE);
-    body -> SetLabel("world");
+    //body -> SetLabel("world");
 
 
     // May not be necessary to bind this to the panel
@@ -104,7 +108,7 @@ public:
     // Bind(wxEVT_TIMER, OnClose);
 
     auto sz_main = new wxBoxSizer(wxVERTICAL);
-    sz_main -> Add(title, 0, wxEXPAND | wxBOTTOM, 5);
+    sz_main -> Add(text_title, 0, wxEXPAND | wxBOTTOM, 5);
     sz_main -> Add(body, 1, wxEXPAND, 0);
 
     auto sub_sizer = new wxBoxSizer(wxVERTICAL);
@@ -145,28 +149,6 @@ CalcOrigin(wxSize size)
 }
 
 
-//static const wxCmdLineEntryDesc g_cmdLineDesc[] =
-//{
-//     { wxCMD_LINE_SWITCH, wxT("h"), wxT("help"), wxT("displays help on the command line parameters"),
-//          wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
-//     { wxCMD_LINE_SWITCH, wxT("t"), wxT("test"), wxT("test switch"),
-//          wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_MANDATORY  },
-//     { wxCMD_LINE_SWITCH, wxT("s"), wxT("silent"), wxT("disables the GUI") },
-//
-//     { wxCMD_LINE_NONE }
-//};
-
-//static const wxCmdLineEntryDesc g_cmdLineDesc[] =
-//{
-//     { wxCMD_LINE_SWITCH, "h", "help", "displays help on the command line parameters",
-//          wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_HELP },
-//     { wxCMD_LINE_SWITCH, "t", "test", "test switch",
-//          wxCMD_LINE_VAL_NONE, wxCMD_LINE_OPTION_MANDATORY  },
-//     { wxCMD_LINE_SWITCH, "s", "silent", "disables the GUI" },
-//
-//     { wxCMD_LINE_NONE }
-//};
-
 class App : public wxApp
 {
 public:
@@ -174,49 +156,38 @@ public:
   App(){};
   ~App(){};
 
-  //void OnInitCmdLine(wxCmdLineParser& parser)
-  //{
-  //    parser.SetDesc(g_cmdLineDesc);
-  //    // must refuse '/' as parameter starter or cannot use "/path" style paths
-  //    parser.SetSwitchChars(wxT("-"));
-  //}
-
-  //bool OnCmdLineParsed(wxCmdLineParser& parser)
-  //{
-
-  //    spdlog::debug("parsing args...");
-  //    bool silent_mode = parser.Found(wxT("s"));
-  //    spdlog::debug("silent mode found: {}", silent_mode);
-
-
-  //    // to get at your unnamed parameters use
-  //    wxArrayString args;
-  //    for (int i = 0; i < parser.GetParamCount(); i++)
-  //    {
-  //        args.Add(parser.GetParam(i));
-  //    }
-
-  //    // and other command line parameters
-  //    for (auto& arg : args) {
-  //        std::cout << arg << std::endl;
-  //        //spdlog::debug("argument: {}", arg.c_str());
-  //    }
-
-  //    // then do what you need with them.
-
-
-  //    return true;
-  //}
+ 
   virtual bool OnInit()
   {
-      auto logger = spdlog::basic_logger_mt("basic_logger", "app.log");
-      logger->set_level(spdlog::level::debug);
-      logger->debug("Logging to file!");
-      spdlog::set_default_logger(std::move(logger));
+      std::vector<spdlog::sink_ptr> sinks = {
+        std::make_shared<spdlog::sinks::basic_file_sink_mt>("app.log", true),
+      };
+
+      auto logger_ = std::make_shared<spdlog::logger>("main", begin(sinks), end(sinks));
+      logger_->set_level(spdlog::level::debug);
+      logger_->set_pattern("[%T][%n][%l] %v"); // "[HH:MM:SS][logger_name][level] msg"
+
+      spdlog::set_default_logger(std::move(logger_));
       spdlog::flush_every(std::chrono::seconds(2));
+      spdlog::debug("Logging to file!");
+
+
+      auto argument_count = wxTheApp->argc;
+      spdlog::debug("argument count: {}", argument_count);
+
+      if (argument_count < 3)
+      {
+          wxLogError("Too few arguments.");
+          return false;
+      }
+
+      auto message = wxTheApp->argv[1];
+      auto title = wxTheApp->argv[2];      
+
       auto size = wxSize(300, 150);
-      frame_ = new Frame(CalcOrigin(size), size);
+      frame_ = new Frame(CalcOrigin(size), size, message, title);
       frame_->Show();
+
       return true;
   }
   // virtual int OnExit();
